8d0aeac073e97409663016a2c48aae99f8c454a3
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-14 12:35:04 +0200

    End

---
+++ source/js/index.js
@@ -0,0 +1,9 @@
+
+// Variable et constante
+const darkMode = document.getElementById('dark-mode');
+
+darkMode.addEventListener('change', () => {
+
+    document.body.classList.toggle('dark');
+
+});
